074431c16eb10d02d03073de29dc6966b4983c68
Sebastian Doe <sebastian.doe24@imperial.ac.uk>
2024-10-19 22:02:13 +0100

    Add detection for cars

--- model/objectDetection.py
+++ model/objectDetection.py
@@ -5,15 +5,22 @@ def objectDetection(imageLocation):
 
     model = torch.hub.load('C:/Users/sebas/Dev/github/treetastic/model/yolov5','custom', path='C:/Users/sebas/Dev/github/treetastic/model/yolov5/last.pt',force_reload=True,source='local')
 
+    modelWithCars = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
     # Images
     imgs = [imageLocation]  # batch of images
 
     # Inference
     results = model(imgs)
+    resultsCars = modelWithCars(imgs)
 
     # Results
     results.print()
     results.save()  # or .show()
+
+    # Results
+    resultsCars.print()
+    resultsCars.save()  # or .show()
 
     results.xyxy[0]  # img1 predictions (tensor)
     results.pandas().xyxy[0]  # img1 predictions (pandas)
@@ -22,7 +29,7 @@ def objectDetection(imageLocation):
     # 1  433.50  433.50   517.5  714.5    0.687988     27     tie
     # 2  114.75  195.75  1095.0  708.0    0.624512      0  person
     # 3  986.00  304.00  1028.0  420.0    0.286865     27     tie
-    print(results.pandas().xyxy[0])
+    [results.pandas().xyxy[0], resultsCars.pandas().xyxy[0]]
 
 # objectDetection("C:/Users/sebas/Downloads/MVIMG_20230317_213244.jpg")
 # objectDetection("C:/Users/sebas/Downloads/Photos/IMG_20241019_121612143.jpg")
